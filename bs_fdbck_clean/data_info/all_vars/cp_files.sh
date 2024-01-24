861d9ad353598f9fb0d51ae55593fab1c85691d5
Sara Marie Blichner <samebr@gmail.com>
2024-01-24 19:00:17 +0100

    Update variable overview info.

---
+++ bs_fdbck_clean/data_info/all_vars/cp_files.sh
@@ -0,0 +1,18 @@
+cp /proj/bolinc/users/x_sarbl/analysis/BS-FDBCK_clean/bs_fdbck_clean/data_info/all_vars/variable_description_dataset/NorESM_clm/noresm_clm.csv /proj/bolinc/users/x_sarbl/analysis/Dataset_BS-FDBCK/NorESM_clm/
+
+cp /proj/bolinc/users/x_sarbl/analysis/BS-FDBCK_clean/bs_fdbck_clean/data_info/all_vars/variable_description_dataset/collocated_ds/NorESM/variable_overview.csv       /proj/bolinc/users/x_sarbl/analysis/Dataset_BS-FDBCK/collocated_ds/NorESM/
+cp /proj/bolinc/users/x_sarbl/analysis/BS-FDBCK_clean/bs_fdbck_clean/data_info/all_vars/variable_description_dataset/collocated_ds/EC-Earth/variable_overview.csv     /proj/bolinc/users/x_sarbl/analysis/Dataset_BS-FDBCK/collocated_ds/EC-Earth/
+cp /proj/bolinc/users/x_sarbl/analysis/BS-FDBCK_clean/bs_fdbck_clean/data_info/all_vars/variable_description_dataset/collocated_ds/ECHAM-SALSA/variable_overview.csv  /proj/bolinc/users/x_sarbl/analysis/Dataset_BS-FDBCK/collocated_ds/ECHAM-SALSA/
+cp /proj/bolinc/users/x_sarbl/analysis/BS-FDBCK_clean/bs_fdbck_clean/data_info/all_vars/variable_description_dataset/collocated_ds/UKESM/variable_overview.csv        /proj/bolinc/users/x_sarbl/analysis/Dataset_BS-FDBCK/collocated_ds/UKESM/
+
+
+cp /proj/bolinc/users/x_sarbl/analysis/BS-FDBCK_clean/bs_fdbck_clean/data_info/all_vars/variable_description_dataset/extracted_latlon_subset/NorESM/variable_overview.csv       /proj/bolinc/users/x_sarbl/analysis/Dataset_BS-FDBCK/extracted_latlon_subset/NorESM/
+cp /proj/bolinc/users/x_sarbl/analysis/BS-FDBCK_clean/bs_fdbck_clean/data_info/all_vars/variable_description_dataset/extracted_latlon_subset/EC-Earth/variable_overview.csv     /proj/bolinc/users/x_sarbl/analysis/Dataset_BS-FDBCK/extracted_latlon_subset/EC-Earth/
+cp /proj/bolinc/users/x_sarbl/analysis/BS-FDBCK_clean/bs_fdbck_clean/data_info/all_vars/variable_description_dataset/extracted_latlon_subset/ECHAM-SALSA/variable_overview.csv  /proj/bolinc/users/x_sarbl/analysis/Dataset_BS-FDBCK/extracted_latlon_subset/ECHAM-SALSA/
+cp /proj/bolinc/users/x_sarbl/analysis/BS-FDBCK_clean/bs_fdbck_clean/data_info/all_vars/variable_description_dataset/extracted_latlon_subset/UKESM/variable_overview.csv        /proj/bolinc/users/x_sarbl/analysis/Dataset_BS-FDBCK/extracted_latlon_subset/UKESM/
+cp /proj/bolinc/users/x_sarbl/analysis/BS-FDBCK_clean/bs_fdbck_clean/data_info/all_vars/variable_description_dataset/extracted_latlon_subset/NorESM/variable_overview.csv       /proj/bolinc/users/x_sarbl/analysis/Dataset_BS-FDBCK/collocated_ds/NorESM/
+
+
+
+
+                    
